db14137b2fd17787081602ea40defafd5a9aa552
Walter Gordy <walter.gordy@gmail.com>
2016-06-16 07:47:36 -0600

    Works on getting post to work

--- ai.r
+++ ai.r
@@ -1,17 +1,20 @@
 #Made with RStudio. R vs 3.2
-
 library(jsonlite)
 library(httr)
-
+library(randomNames)
 
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 base.url <- "http://www.earthempires.com/api"
-user.name <- "username"
-api.key <- "apikey"
+user.name <- "salted"
+api.key <- ""
 
-default.params <- paste("?", "username=", user.name, "&ai_key=", api.key, "&server=ai", sep="")
+#default.params <- paste("?", "username=", user.name, "&ai_key=", api.key, "&server=ai", sep="")
+default.params <- list()
+default.params$username <- user.name
+default.params$ai_key <- api.key
+default.params$server <- "ai"
 
 fixjson <- function(json)
 {
@@ -20,32 +23,54 @@ fixjson <- function(json)
   return(json)
 }
 
-doPOST <- function(function.url="info", body)
+doPOST <- function(params)
 {
-  url <- paste(base.url, "/", function.url, default.params, sep="")
+  
+  
+  url <- paste(base.url, "?api_payload=", p, sep="")
   print(url)
-  req <- GET(url=url)
+  
+  p <- toJSON(params, auto_unbox=TRUE)
+  
+  req <- POST(url=url)
   json <- content(req, "text")
-  
+  print(json)
   json <- fixjson(json)
-  
   return(json)
 }
 
-doPOST()
-
 getInfo <- function()
 {
-  
-  
+  params <- default.params
+  params$api_function <- "info"
+  res <- doPOST(params)
+  return(fromJSON(res))
 }
 
-res <- doPOST()
+getServer <- function()
+{
+  params <- default.params
+  params$api_function <- "server"
+  res <- doPOST(params)
+  return(fromJSON(res))
+}
 
-api_function=server&api_payload='{"username":"salted","ai_key":"49ee125ad5e9a3b81dfb771ac0d3d2fb","server":"ai"}'
+createCountry <- function()
+{
+  params <- default.params
+  params$api_function <- "create"
+  params$cname <- paste(sample(state.division, 1), randomNames(n=1,which.names="first"))
+  res <- doPOST(params)
+  return(fromJSON(res))
+}
 
-req <- curl_fetch_memory("http://www.earthempires.com/api/server?username=salted&ai_key=49ee125ad5e9a3b81dfb771ac0d3d2fb&server=ai")
-req <- POST()
-parse_headers(req$headers)
-cat(rawToChar(req$content))
+res <- POST(url="http://www.earthempires.com/api/info", body='api_function=info&api_payload={"username":"salted","ai_key":"49ee125ad5e9a3b81dfb771ac0d3d2fb","server":"ai"}', encode="json", verbose())
+content(res, "text")
 
+
+#info <- getInfo()
+#server <- getServer()
+
+
+
+#country <- createCountry()

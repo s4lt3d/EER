8f80c27a4c1e7be0aecba641cae29dc3521a3620
Walter Gordy <walter.gordy@gmail.com>
2016-07-15 17:43:17 -0600

    git

--- ai.r
+++ ai.r
@@ -33,11 +33,6 @@ advisor.history <- tbl_df(advisor.history)
 # get only the current round for the bots
 getInfo()
 
-<<<<<<< Updated upstream
-=======
-for(i in 1:35) createCountry()
-
->>>>>>> Stashed changes
 repeat
 {
   for(i in 1:25) createCountry()
